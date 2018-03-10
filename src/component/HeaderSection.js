f51b3f885034674ae8299e64bb0ec513488b2d2d
Ali Almohaya <alialmohaya@gmail.com>
2018-03-10 19:26:26 +0300

    select all the colors

--- src/component/HeaderSection.js
+++ src/component/HeaderSection.js
@@ -7,12 +7,12 @@ export default class HeaderSection extends Component {
   render() {
     return (<div className='HeaderSection'>
 
-      <p>const aboutMe = &#123;</p>
-      <h3>name: 'Ali Almohaya',</h3>
-      <h3>skills: 'Web Dev/Des',</h3>
-      <h3>birth: '1996/12/15',</h3>
-      <h3>based: 'Yemen/Sanna',</h3>
-      <p>&#125;;</p>
+      <p><span className='stat'>const</span> aboutMe <span className='stat'> = </span> &#123;</p>
+      <h3>name<span className='stat'>:</span> <span className='stat'>'</span>Ali Almohaya<span className='stat'>'</span> ,</h3>
+      <h3>skills<span className='stat'>:</span> <span className='stat'>'</span>Web Dev/Des<span className='stat'>'</span>,</h3>
+      <h3>birth<span className='stat'>:</span> <span className='stat'>'</span>1996/12/15<span className='stat'>'</span>,</h3>
+      <h3>based<span className='stat'>:</span> <span className='stat'>'</span>Yemen/Sanna<span className='stat'>'</span>,</h3>
+      <p className='stat'>&#125;;</p>
 
     </div>);
   }
