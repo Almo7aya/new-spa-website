78d9771b66ca6f1c3880145a6e46a51bb54ab4e8
Ali Almohaya <alialmohaya@gmail.com>
2018-03-10 18:26:16 +0300

    add footer component

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
+import '../styles/Footer.css';
 
 export default class Footer extends Component {
+
+  render() {
+    return (<div className='Footer'>
+
+      <div id='inter'>kl
+        <span></span>
+        <span></span>
+      </div>
+
+      <div id='icons'>
+
+      </div>
+
+    </div>);
+  }
 
 }
