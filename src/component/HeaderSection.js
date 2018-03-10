255fd58efea53f1e98a0eb001d2182e5a61f4194
Ali Almohaya <alialmohaya@gmail.com>
2018-03-10 18:21:19 +0300

    change text and font - add footer component

--- src/component/HeaderSection.js
+++ src/component/HeaderSection.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 
+import '../styles/HeaderSection.css';
+
 export default class HeaderSection extends Component {
 
   render() {
-    return (<div>
+    return (<div className='HeaderSection'>
 
-      <p>WhoIs () &#123;</p>
-      <h1>ALI ALMOHAYA</h1>
-      <p>&#125;</p>
+      <p>const aboutMe = &#123;</p>
+      <h3>name: 'Ali Almohaya',</h3>
+      <h3>skills: 'Web Dev/Des',</h3>
+      <h3>birth: '1996/12/15',</h3>
+      <h3>based: 'Yemen/Sanna',</h3>
+      <p>&#125;;</p>
 
     </div>);
   }
 
 }
+
+
+const aboutMe = {
+  name: 'Ali Almohaya',
+  skills: 'Web Dev/Des',
+  dayOfBirth: '1996/12/15',
+};
