956ef001c7a966dc801fcb1ce960b3e77d2d4bb3
Ali Almohaya <alialmohaya@gmail.com>
2018-03-11 17:31:45 +0300

    support PWA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import serviceWorkerRegister from './utils/registerServiceWorker';
+
 // main styles
 import './styles/index.css'
 // components 
@@ -8,3 +10,6 @@ import App from './component/App';
 
 // start point
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// create sw.js file
+serviceWorkerRegister();
