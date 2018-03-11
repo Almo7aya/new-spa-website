3fdb4051b0c9e153c818d76824f12d8b0d8407ea
Ali Almohaya <alialmohaya@gmail.com>
2018-03-11 18:03:59 +0300

    Add tags component

---
+++ src/component/Tags.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import '../styles/Tags.css';
+
+export default class Tags extends Component {
+
+  render() {
+    return (<div className='Tags' >
+      <p>#Angular</p>
+      <p>#React</p>
+      <p>#Redux</p>
+      <p>#PWA</p>
+      <p>#Node.js</p>
+    </div>);
+  }
+
+}
