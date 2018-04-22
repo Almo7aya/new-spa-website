a54fd37d464060fc40d9012822b827666f5b4272
Ali Almohaya <AliAlmohaya@gmail.com>
2018-04-22 19:32:58 +0300

    change the test to fullstack

--- src/component/HeaderSection.js
+++ src/component/HeaderSection.js
@@ -9,7 +9,7 @@ export default class HeaderSection extends Component {
 
       <p><span className='stat'>const</span> aboutMe <span className='stat'> = </span><span className='stat'> &#123;</span></p>
       <h3>name<span className='stat'>:</span> <span className='stat'>'</span>Ali Almohaya<span className='stat'>'</span>,</h3>
-      <h3>skills<span className='stat'>:</span> <span className='stat'>'</span>Web/Mob dev<span className='stat'>'</span>,</h3>
+      <h3>skills<span className='stat'>:</span> <span className='stat'>'</span>fullstack dev<span className='stat'>'</span>,</h3>
       <h3>birth<span className='stat'>:</span> <span className='stat'>'</span>1996/12/15<span className='stat'>'</span>,</h3>
       <h3>based<span className='stat'>:</span> <span className='stat'>'</span>Yemen/Sanna<span className='stat'>'</span>,</h3>
       <p className='stat'>&#125;;</p>
